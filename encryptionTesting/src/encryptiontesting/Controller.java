47ca54f606e279956aac9a0f6c1ae86b55cabcbe
mpoxson <mpoxson@gmail.com>
2023-04-01 17:58:54 -0400

    HBOX

--- encryptionTesting/src/encryptiontesting/Controller.java
+++ encryptionTesting/src/encryptiontesting/Controller.java
@@ -7,6 +7,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -15,13 +17,60 @@ import javafx.stage.Stage;
 
 public class Controller extends Application {
     private int key;
+    public TextField txt = new TextField();
+    private HBox encBox;
     
     @Override
     public void start(Stage primaryStage) {
-
-        GridPane root = new GridPane(); 
-        HBox box = addHBox();
-        root.getChildren().add(box);
+        HBox decBox;
+        HBox hbox = new HBox();
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #336699;");
+        GridPane root = new GridPane();
+//        Button buttonCurrent = new Button("Current");
+//        buttonCurrent.setPrefSize(100, 20);
+//
+//        Button buttonProjected = new Button("Projected");
+//        buttonProjected.setPrefSize(100, 20);
+//        hbox.getChildren().addAll(buttonCurrent, buttonProjected);
+        
+        EncryptDecrypt using = new EncryptDecrypt();
+        Button enc = new Button();
+        enc.setText("ENCRYPTION");
+        enc.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                encBox = encHBox();
+                root.add(encBox, 1, 2);
+            }
+        });
+        
+        Button dec = new Button();
+        dec.setText("DECRYPTION");
+        dec.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                using.decrypt();
+            }
+        });
+        
+        Button quit = new Button();
+        quit.setText("QUIT");
+        quit.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Quitting");
+                System.exit(0);  
+            }
+        });
+        
+        hbox.getChildren().addAll(enc,dec,quit);
+        System.out.println("Added");
+         
+        root.add(hbox,1,1);
         Scene scene = new Scene(root, 500, 500);
         
         primaryStage.setTitle("Encryption/Decryption");
@@ -36,10 +85,8 @@ public class Controller extends Application {
         launch(args);
     }
     
-    public HBox addHBox() {
+    public HBox encHBox() {
         HBox hbox = new HBox();
-        hbox.setSpacing(10);
-        hbox.setStyle("-fx-background-color: #336699;");
 
 //        Button buttonCurrent = new Button("Current");
 //        buttonCurrent.setPrefSize(100, 20);
@@ -50,7 +97,7 @@ public class Controller extends Application {
         
         EncryptDecrypt using = new EncryptDecrypt();
         Button enc = new Button();
-        enc.setText("ENCRYPTION");
+        enc.setText("ENCRYPT");
         enc.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
@@ -59,82 +106,15 @@ public class Controller extends Application {
             }
         });
         
-        Button dec = new Button();
-        dec.setText("DECRYPTION");
-        dec.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                using.decrypt();
-            }
-        });
+        Label lab = new Label();
+        lab.setText("Key: ");
+        txt.setText("Waiting for encryption to complete");
         
-        Button quit = new Button();
-        quit.setText("QUIT");
-        quit.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Quitting");
-                System.exit(0);  
-            }
-        });
-        
-        hbox.getChildren().addAll(enc,dec,quit);
+        hbox.getChildren().addAll(enc,lab,txt);
         
 
     return hbox;
-    }
- 
-    public HBox addEnc() {
-        HBox hbox = new HBox();
-        hbox.setSpacing(10);
-        hbox.setStyle("-fx-background-color: #336699;");
-
-//        Button buttonCurrent = new Button("Current");
-//        buttonCurrent.setPrefSize(100, 20);
-//
-//        Button buttonProjected = new Button("Projected");
-//        buttonProjected.setPrefSize(100, 20);
-//        hbox.getChildren().addAll(buttonCurrent, buttonProjected);
-        
-        EncryptDecrypt using = new EncryptDecrypt();
-        Button enc = new Button();
-        enc.setText("ENCRYPTION");
-        enc.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                key = using.encrypt();
-            }
-        });
-        
-        Button dec = new Button();
-        dec.setText("DECRYPTION");
-        dec.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                using.decrypt();
-            }
-        });
-        
-        Button quit = new Button();
-        quit.setText("QUIT");
-        quit.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Quitting");
-                System.exit(0);  
-            }
-        });
-        
-        hbox.getChildren().addAll(enc,dec,quit);
-        
-
-    return hbox;
-    }    
+    }  
     
     
 }
